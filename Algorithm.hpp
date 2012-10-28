3284d6325e1c84f686bd69af0a226d8a60f7d3a2
Atila Neves <devnull@localhost>
2012-10-28 13:47:33 +0100

    Implemented run in Alg

--- Algorithm.hpp
+++ Algorithm.hpp
@@ -8,14 +8,16 @@ namespace ga {
     template<typename GENE = bool, class CONTAINER = std::vector<GENE>>
     class Algorithm {
     public:
+	typedef Individual<GENE, CONTAINER> MyIndividual;
+
 	Algorithm(unsigned populationSize, unsigned genomeSize);
 
 	template<class FITNESS, class SELECT, class XOVER, class MUTATE>
-	void run(const FITNESS& fitness, const SELECT& select,
-                 const XOVER& xover, const MUTATE& mutate);
+	MyIndividual* run(const FITNESS& fitness, const SELECT& select,
+			  const XOVER& xover, const MUTATE& mutate);
 
     private:
-	typedef std::vector<Individual<GENE, CONTAINER>> Population;
+	typedef std::vector<MyIndividual> Population;
 
 	Population _population;
     };
@@ -26,6 +28,15 @@ namespace ga {
 	    _population.push_back(Individual<GENE, CONTAINER>());
 	}
     }
+
+    template<typename GENE, class CONTAINER>
+    template<class FITNESS, class SELECT, class XOVER, class MUTATE>
+    typename Algorithm<GENE, CONTAINER>::MyIndividual* //return value
+    Algorithm<GENE, CONTAINER>::run(const FITNESS& fitness, const SELECT& select,
+				    const XOVER& xover, const MUTATE& mutate) {
+	return nullptr;
+    }
+
 
 }
 

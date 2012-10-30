5ec429b592fce2646ab427407516821c7bc7a467
Atila Neves <aalvesne@cisco.com>
2012-10-30 12:26:43 +0100

    Made Algorithm depend on only one template parameter

--- Algorithm.hpp
+++ Algorithm.hpp
@@ -10,40 +10,38 @@
 #include <map>
 
 namespace ga {
-
-    template<typename GENE = bool, class CONTAINER = std::vector<GENE>>
+template<class INDIVIDUAL = Individual<>>
     class Algorithm {
     public:
-        typedef Individual<GENE, CONTAINER> MyIndividual;
-        typedef std::function<double(const MyIndividual&)> FitnessFunc;
-        typedef typename MyIndividual::MutateFunc MutateFunc;
-        typedef typename MyIndividual::XoverFunc XoverFunc;
-        typedef typename SelectParents<MyIndividual>::Rankings Rankings;
-        typedef typename SelectParents<MyIndividual>::ParentTuple ParentTuple;
-        typedef typename SelectParents<MyIndividual>::SelectFunc SelectFunc;
+        typedef std::function<double(const INDIVIDUAL&)> FitnessFunc;
+        typedef typename INDIVIDUAL::MutateFunc MutateFunc;
+        typedef typename INDIVIDUAL::XoverFunc XoverFunc;
+        typedef typename SelectParents<INDIVIDUAL>::Rankings Rankings;
+        typedef typename SelectParents<INDIVIDUAL>::ParentTuple ParentTuple;
+        typedef typename SelectParents<INDIVIDUAL>::SelectFunc SelectFunc;
         
         Algorithm(unsigned populationSize, unsigned genomeSize);
         
-        const MyIndividual& run(double fitness, const FitnessFunc& fitnessFunc, const SelectFunc& select,
-                                const XoverFunc& xover, const MutateFunc& mutate);
-        const MyIndividual& run(double fitness, const FitnessFunc& fitnessFunc,
-                                double mutateRate, double xoverRate = 1.0);
+        const INDIVIDUAL& run(double fitness, const FitnessFunc& fitnessFunc, const SelectFunc& select,
+                              const XoverFunc& xover, const MutateFunc& mutate);
+        const INDIVIDUAL& run(double fitness, const FitnessFunc& fitnessFunc,
+                              double mutateRate, double xoverRate = 1.0);
         
     private:
-        typedef std::vector<MyIndividual> Population;
+        typedef std::vector<INDIVIDUAL> Population;
 
         Population _population;
 
-        const MyIndividual& getFittest(const FitnessFunc& fitnessFunc) const {
+        const INDIVIDUAL& getFittest(const FitnessFunc& fitnessFunc) const {
             return *std::max_element(_population.cbegin(), _population.cend(),
-                                     [&](const MyIndividual& i1, const MyIndividual& i2) {
+                                     [&](const INDIVIDUAL& i1, const INDIVIDUAL& i2) {
                                          return fitnessFunc(i1) < fitnessFunc(i2); });
         }
 
         double getHighestFitness(const FitnessFunc& fitnessFunc) const {
             std::vector<double> values(_population.size());
             std::transform(_population.cbegin(), _population.cend(), values.begin(),
-                           [&](const MyIndividual& i) { return fitnessFunc(i); });
+                           [&](const INDIVIDUAL& i) { return fitnessFunc(i); });
             return *std::max_element(values.cbegin(), values.cend());
         }
 
@@ -58,21 +56,22 @@ namespace ga {
 
         void printGeneration(int generation) const {
             std::cout << "Generation " << generation << std::endl;
-            for(const MyIndividual& ind: _population) std::cout << ind;
+            for(const INDIVIDUAL& ind: _population) std::cout << ind;
             std::cout << std::endl;
         }
     };
 
-    template<typename GENE, class CONTAINER>
-    Algorithm<GENE, CONTAINER>::Algorithm(unsigned populationSize, unsigned genomeSize) {
+    template<class INDIVIDUAL>
+    Algorithm<INDIVIDUAL>::Algorithm(unsigned populationSize, unsigned genomeSize) {
         for(unsigned i = 0; i < populationSize; ++i) {
-            _population.push_back(MyIndividual(genomeSize));
+            _population.push_back(INDIVIDUAL(genomeSize));
         }
     }
 
-    template<typename GENE, class CONTAINER>
-    auto Algorithm<GENE, CONTAINER>::run(double fitness, const FitnessFunc& fitnessFunc, const SelectFunc& select,
-                                         const XoverFunc& xover, const MutateFunc& mutate) -> const MyIndividual& {
+    template<class INDIVIDUAL>
+    const INDIVIDUAL& Algorithm<INDIVIDUAL>::run(double fitness, const FitnessFunc& fitnessFunc,
+                                                 const SelectFunc& select,
+                                                 const XoverFunc& xover, const MutateFunc& mutate) {
         int generation = 0;
         while(getHighestFitness(fitnessFunc) < fitness) {
             printGeneration(generation);
@@ -82,7 +81,7 @@ namespace ga {
             while(newPopulation.size() < _population.size()) {
                 const auto ranked = rankPopulation(fitnessFunc);
                 const auto parents = select(ranked);
-                auto children = MyIndividual::createChildren(std::get<0>(parents), std::get<1>(parents),
+                auto children = INDIVIDUAL::createChildren(std::get<0>(parents), std::get<1>(parents),
                                                              xover, mutate);
                 newPopulation.push_back(std::move(std::get<0>(children)));
                 newPopulation.push_back(std::move(std::get<1>(children)));
@@ -95,12 +94,12 @@ namespace ga {
         return getFittest(fitnessFunc);
     }
 
-    template<typename GENE, class CONTAINER>
-    auto Algorithm<GENE, CONTAINER>::run(double fitness, const FitnessFunc& fitnessFunc,
-                                         double mutateRate, double xoverRate) -> const MyIndividual& {
-        return run(fitness, fitnessFunc, Tournament<MyIndividual>(),
-                   SinglePointCrossover<MyIndividual>(xoverRate),
-                   Mutate<MyIndividual>(mutateRate));
+    template<class INDIVIDUAL>
+    const INDIVIDUAL& Algorithm<INDIVIDUAL>::run(double fitness, const FitnessFunc& fitnessFunc,
+                                                 double mutateRate, double xoverRate) {
+        return run(fitness, fitnessFunc, Tournament<INDIVIDUAL>(),
+                   SinglePointCrossover<INDIVIDUAL>(xoverRate),
+                   Mutate<INDIVIDUAL>(mutateRate));
     }
 }
 
